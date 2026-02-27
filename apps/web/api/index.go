c28d1c52e5c0a516b263a0717d186992421d7138
tmnts <annashulik1994@gmail.com>
2026-02-27 12:57:28 +0300

    feat: finalize full-stack architecture with Swagger docs

--- apps/web/api/index.go
+++ apps/web/api/index.go
@@ -1,15 +1,36 @@
+// @title Weather-Flex API
+// @version 1.0
+// @description This is a Go-powered weather proxy for OpenWeatherMap.
+// @host localhost:3000
+// @BasePath /
 package handler // Vercel NEEDS this to be "handler"
 
 import (
+	httpSwagger "://github.com"
 	"io"
 	"net/http"
 	"os"
+	"strings"
+	_ "weather-api/docs"
+
+	httpSwagger "github.com/swaggo/swag/cmd/swag@latest"
 )
 
+// Handler godoc
+// @Summary Get current weather
+// @Description Fetch real-time weather data for Khimki via OpenWeatherMap
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router /weather [get]
 func Handler(w http.ResponseWriter, r *http.Request) {
 	apiKey := os.Getenv("OPENWEATHER_API_KEY")
 	// Make sure this URL is 100% correct with your lat/lon
 	apiUrl := "https://api.openweathermap.org/data/2.5/weather?q=moscow&units=metric&appid=" + apiKey
+
+	if strings.Contains(r.URL.Path, "swagger") {
+		httpSwagger.WrapHandler(w, r)
+		return // Exit here so we don't run the weather code
+	}
 
 	resp, err := http.Get(apiUrl)
 	if err != nil {
